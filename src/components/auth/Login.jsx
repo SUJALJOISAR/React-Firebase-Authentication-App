bb724c9ac2eb5d1ce00ca2042aff756758a34f7d
SUJALJOISAR <sujaljoisar@gmail.com>
2024-05-16 00:31:35 +0530

    google authentication with firebase completed

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -5,8 +5,11 @@ import { Link,useNavigate } from 'react-router-dom';
 import IMG2 from '../photos/back_arrow.png'; 
 import {toast} from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import { signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 import { auth } from '../../firebase/firebase';
+import { GoogleAuthProvider } from 'firebase/auth';
+
+
 
 const Login = () => {
     const [email,setEmail] =useState("");
@@ -39,6 +42,23 @@ const Login = () => {
         }
     }
 
+    function googleLogin(){
+        const provider = new GoogleAuthProvider();
+         signInWithPopup(auth,provider).then(async (result)=>{
+            console.log(result.user);
+            if(result.user){
+                toast.success("User logged in Successfully",{
+                    position:"top-center",
+                    autoClose:8000,
+                    pauseOnHover:true,
+                    draggable:true,
+                    theme:"dark",
+                });
+            }
+            navigate("/");
+         });
+    }
+
   return (
     <>
       <section className="bg-gray-50 min-h-screen flex items-center justify-center">
@@ -66,7 +86,7 @@ const Login = () => {
               <hr className="text-gray-400"/>
             </div>
 
-            <button className="bg-white border py-2 w-full rounded-xl mt-3 flex justify-center items-center text-md hover:scale-105 duration-300">
+            <button className="bg-white border py-2 w-full rounded-xl mt-3 flex justify-center items-center text-md hover:scale-105 duration-300" onClick={googleLogin}>
             <svg className="mr-3" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20px" height="25px" viewBox="0 0 48 48">
                 <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path><path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path><path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"></path><path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"></path>
               </svg>Login With Google</button>
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -33,14 +33,20 @@ const Home = () => {
 
     return (
         <div className="min-h-screen flex items-center justify-center">
-            <div className="bg-gray-100 rounded-lg shadow-lg p-6">
+            <div className="bg-gray-100 rounded-lg shadow-lg p-10">
             <h3 className="text-2xl font-semibold mb-4">Welcome to Axire Infotech</h3>
                 {userDetails ? (
                     <>
-                        <div>
-                            <h4 className="text-lg">Username: {userDetails.email}</h4>
+                        <div style={{
+                            display: "flex",
+                            justifyContent:"center"
+                        }}>
+                            <img src={userDetails.photoURL} alt="" style={{
+                                "borderRadius":"50%",
+                                "width":"20%"}}/>
+                            <h4 className="text-lg py-4 ml-2">Username: {userDetails.email}</h4>
                         </div>
-                         <button className='btn btn-primary mt-4' onClick={handleLogout}>
+                         <button className='btn btn-primary mt-4 ml-20' onClick={handleLogout}>
                             Logout
                         </button>
                     </>
